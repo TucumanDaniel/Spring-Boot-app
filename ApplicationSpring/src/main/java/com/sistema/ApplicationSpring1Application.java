8b58f6d9e0e057fd9d17c30d2bb7aa37e502167c
Daniel <daniel.alberto.danielsen@hotmail.com>
2021-12-02 11:32:14 -0300

    Comit inicial con eclipse

---
+++ ApplicationSpring/src/main/java/com/sistema/ApplicationSpring1Application.java
@@ -0,0 +1,13 @@
+package com.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationSpring1Application.class, args);
+	}
+
+}

2f1dca37762a4fd4ee3d4ea51e4305587d424780
Daniel <daniel.alberto.danielsen@hotmail.com>
2021-12-29 11:15:48 -0300

    Security but dont compile.

--- ApplicationSpring/src/main/java/com/sistema/service/UserServiceImpl.java
+++ ApplicationSpring/src/main/java/com/sistema/service/UserServiceImpl.java
@@ -3,6 +3,10 @@ package com.sistema.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.sistema.dto.ChangePasswordForm;
@@ -15,6 +19,9 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	UserRepository repository;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Override
 	public Iterable<User> getAllUsers() {
@@ -76,6 +83,7 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
 	public void deleteUser(Long id) throws Exception {
 		User user = getUserById(id);
 		repository.delete(user);
@@ -86,7 +94,7 @@ public class UserServiceImpl implements UserService{
 	public User changePassword(ChangePasswordForm form) throws Exception{
 		User user = getUserById(form.getId());
 		
-		if( !user.getPassword().equals(form.getCurrentPassword())) {
+		if( !isLoggedUserADMIN() && !user.getPassword().equals(form.getCurrentPassword())) {
 			throw new Exception("Current Password Incorrect.");
 		}
 		
@@ -98,9 +106,27 @@ public class UserServiceImpl implements UserService{
 			throw new Exception("New Password and Confirm Password does not match!");
 		}
 		
-		user.setPassword(form.getNewPassword());
+		String encodePassword = bCryptPasswordEncoder.encode(form.getNewPassword());
+		user.setPassword(encodePassword);
 		return repository.save(user);
 	}
 	
+	public boolean isLoggedUserADMIN(){
+		 return loggedUserHasRole("ROLE_ADMIN");
+		}
+
+	public boolean loggedUserHasRole(String role) {
+		 Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		 UserDetails loggedUser = null;
+		 Object roles = null; 
+		 if (principal instanceof UserDetails) {
+		  loggedUser = (UserDetails) principal;
+		 
+		  roles = loggedUser.getAuthorities().stream()
+		    .filter(x -> role.equals(x.getAuthority() ))      
+		    .findFirst().orElse(null); //loggedUser = null;
+		 }
+		 return roles != null ?true :false;
+		}
 
 }
